8f467e037b6322401b8e7586a52e0269ba5f9aea
husamabdel <81593002+husamabdel@users.noreply.github.com>
2021-10-22 12:34:51 -0400

    Update LAUNCH.java

--- LAUNCH.java
+++ LAUNCH.java
@@ -1,3 +1,5 @@
+
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Date;
 import java.util.Random;
@@ -7,7 +9,9 @@ import java.net.URI;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.event.*;
-public class LAUNCH extends JFrame{
+import java.awt.desktop.*;
+import java.awt.*;
+public class TASK_LAUNCH extends JFrame{
 
     //All variables, plan is to have a grid layout, three horizontal panels, middle has the ticket adder, bottom has HOTLINKS, Top has.. well.. no plan for that yet.
 	private JPanel panel;
@@ -18,53 +22,72 @@ public class LAUNCH extends JFrame{
 	private JButton page4;
     private JButton action;
     private JButton action2;
+    private JButton addTicket;
 	private JTextField text;
 	private int width = 750;
 	private int height = 500;
 	private String path;
 	private static boolean flag;
-	//GUI Initializer
-	public LAUNCH() {
+	//GUI Initializer constructor. 
+	public TASK_LAUNCH() {
 		
 		this.setTitle("Ticket Logger MIL");
 		this.setSize(width,height);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(true);
-		this.setVisible(true);
+        this.setResizable(true);
+        this.setBackground(Color.RED);
+        launcher();
+        add(panel);
+        this.setVisible(true);
 	}
     
     
     
-    //HOTLINKS.
+    //HOTLINKS (Mostly label components).
 	public void launcher() {
         
         text = new JTextField(12);
-
+						/* 
+						
+	TODO: Fix this f**king sh*t, the level of fustration is unbearable right now because
+	this f**king seBackground function will not fucking work. Why?
+						
+						*/
         page = new JButton("New Ticket!");
-        page.setBackground(Color.ORANGE);
-        page.setForeground(Color.BLACK);
+     //   page.setBackground(Color.ORANGE);
+     //   page.setForeground(Color.BLACK);
         page.addActionListener(new service());
         //First Button Initialized..
 
         page2 = new JButton("Idaptive Page");
-        page2.setBackgroud(Color.GREEN);
-        page2.setForeground(Color.BLACK);
-        page2.addActionListener(new Idaptive());
+    //    page2.setBackgroud(Color.GREEN);
+    //    page2.setForeground(Color.BLACK);
+        page2.addActionListener(new idaptive());
         //Second Button Initialized.
 
         action = new JButton("ALL DATA");
-        action.setBackground(Color.BLACK);
-        action.seForeground(Color.BLACK);
+     //   action.setBackground(Color.BLACK);
+     //   action.seForeground(Color.BLACK);
         action.addActionListener(new TASK_GET_TEXT_ARRAY());
         //Third Button Initialized, this is for the text file data.
 
         action2 = new JButton("Full Logs");
-        action2.setBackground(Color.WHITE);
-        action2.setForeground(Color.BLACK);
+       // action2.setBackground(Color.WHITE);
+    //    action2.setForeground(Color.BLACK);
         action2.addActionListener(new TASK_GET_TEXT_FILE());
 
+        addTicket = new JButton("Save Ticket");
+    //    addTicket.setBackground(Color.BLUE);
+    //    addTicket.setForeground(Color.BLACK);
+        addTicket.addActionListener(new TASK_ADD_TICKET());
+
         panel = new JPanel();
-		panel.setLayout(new BorderLayout());
+        panel.add(text);
+        panel.add(page);
+        panel.add(page2);
+        panel.add(action);
+        panel.add(action2);
+        panel.add(addTicket);
 		
 	}
     
@@ -82,7 +105,7 @@ public class LAUNCH extends JFrame{
 
 
     //Uses boolean flag to determin path and data and how to use the NEW_TICKET object.
-    public void addTickets(){
+  /*  public void addTickets(){
 
         String username = System.getProperty("user.name");
         String ans;
@@ -103,14 +126,14 @@ public class LAUNCH extends JFrame{
                 System.exit(0);
                 }
             }
+                */
 
-
-    //How about an array of all the data?
+    //How about an array of all the data i want to append?
     
     public void FUNCTION_ADD_ELEMENTS(String data, boolean addFlag){
-        ArrayList <String> datas = new ArrayList<>();
+        ArrayList <String> datas = new ArrayList <String>();
             while(true){
-                datas.add(data);
+                datas.add(data); //Don't know if this wil work...
                     if(addFlag == false){ break; }
                 }
             }
@@ -120,7 +143,7 @@ public class LAUNCH extends JFrame{
 	private class service implements ActionListener{
         public void actionPerformed(ActionEvent e){
             try{
-			    URI link = new URI("https://loc.servicenowservices.com");
+			    URI link = new URI("https://loc.servicenowservices.com/nav_to.do?uri=%2Fincident.do%3Fsys_id%3D-1%26sysparm_query%3Dactive%3Dtrue%26sysparm_stack%3Dincident_list.do%3Fsysparm_query%3Dactive%3Dtrue");
                 java.awt.Desktop.getDesktop().browse(link);
 			    JOptionPane.showMessageDialog(null, "New Ticket Page launched!", "Webpage Message", JOptionPane.ERROR_MESSAGE);}
 			catch(Exception d){
@@ -132,8 +155,8 @@ public class LAUNCH extends JFrame{
 	private class idaptive implements ActionListener{
         public void actionPerformed(ActionEvent e){
             try{
-                URI link = new URI("https://loc.my.idaptive.app");
-                java.awt.desktop.getDesktop().browse(link);
+                URI Ilink = new URI("https://loc.my.idaptive.app");
+                java.awt.desktop.getDesktop().browse(Ilink);
                 JOptionPane.showMessageDialog(null, "Idaptive Page Lauched!", "Webpage Message", JOptionPane.OK_OPTION);
             }
             catch(Exception x){
@@ -154,9 +177,10 @@ public class LAUNCH extends JFrame{
     private class TASK_GET_TEXT_FILE implements ActionListener{
         public void actionPerformed(ActionEvent e){
 
+            String user;
             Desktop desktop = Desktop.getDesktop();
-            String User = System.getProperties(user.name);
-            File file = new File("C:\\Users\\"+User+"\\Desktop\\TODAY.txt");
+            user = System.getProperty("user.name");
+            File file = new File("C:\\Users\\"+user+"\\Desktop\\TODAY.txt");
                 if(file.exists()){
                     desktop.open(file);
                 }
@@ -165,12 +189,39 @@ public class LAUNCH extends JFrame{
                     }
             }
         }
+
+        private class TASK_ADD_TICKET implements ActionListener{
+            public void actionPerformed(ActionEvent e){
+                
+
+                    String username = System.getProperty("user.name");
+                    String ans;
+                    String data = text.getText();
+                    SD_MIL existingFile = new SD_MIL();
+                    if(PATH_DATA() == true) {existingFile.fileStart(data);}
+                        else if(PATH_DATA() == false){
+                            ans = JOptionPane.showInputDialog(null, "Would you like to start a new file?", "FILE_!", JOptionPane.OK_CANCEL_OPTION);
+                                if(ans.equalsIgnoreCase("y") || and.equalsIgnoreCase("yes")){
+                                    existingFile.fileOpen("C:\\Users\\"+username+"\\Documents\\TASK_DATA.txt", data);
+                                    }
+                                    else if(ans.equalsIgnoreCase("n") || ans.equalsIgnoreCase("no")){
+                                        JOptionPane.showMessageDialog(null, "warning! the program will now shut down!", "Warning!", JOptionPane.ERROR_MESSAGE);
+                                        System.exit(0);
+                                        }
+                                    }
+                            else{
+                            System.exit(0);
+                            }
+                        
+            }
+        }
 	//Why do I need to return a boolean when I can just check for file Existence? SMH
 	
 
 	public static void main(String[] args) {
-		
-
+		PATH_DATA();
+        new TASK_LAUNCH();
+        //So far doe not work.. I DONT WANT TO WRITE UI's EVER AGAIN!!
 	}
 
 }
