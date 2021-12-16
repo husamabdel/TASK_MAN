9394a47030cb5de08aa39e7882c9dd2b156b97d1
husamabdel <81593002+husamabdel@users.noreply.github.com>
2021-12-16 09:55:17 -0500

    Update VIEW.java

--- VIEW.java
+++ VIEW.java
@@ -1,50 +1,57 @@
 import javax.swing.*;
-import javax.swing.text.View;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Scanner;
+/*
+*       THIS CLASS WILL BE LINKED TO MAIN (LAUNCH)
+*       The main method here is only for testing.
+*       Class will be used to view elements in array from the main.
+*
+*/
 
 public class VIEW extends JFrame{
 
     
-    public String[] selected;
+    public static String[] selected;
     private JPanel panel;
     private JPanel panel2;
     private JPanel panel3;
-    private JList elementList; // To hold months
-    private JList selectedElementList; // Selected months
+    private JList<String> elementList; // To hold months
+    private JList<String> selectedElementList; // Selected months
     private JScrollPane scrollPane1; // Scroll pane - first list
     private JScrollPane scrollPane2; // Scroll pane - second list
     private JButton button; // A button
+    private DefaultListModel<String> model = new DefaultListModel<>();
 
 
     public VIEW() throws IOException{
 
-        setArray();
-        setTitle("View Data");
-        setLayout(new BorderLayout());
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //pack();
-        setResizable(true);
+        
+        this.setTitle("View Data");
+        this.setLayout(new BorderLayout());
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pack();
+        //this.setSize(500, 500);
+        this.setResizable(true);
 
         setPanel1();
         setPanel2();
         setPanel3();
 
-        add(panel, BorderLayout.NORTH);
-        add(panel2, BorderLayout.CENTER);
-        add(panel3, BorderLayout.SOUTH);
+        this.add(panel, BorderLayout.NORTH);
+        this.add(panel2, BorderLayout.CENTER);
+        this.add(panel3, BorderLayout.SOUTH);
 
-        setVisible(true);
+        this.setVisible(true);
 
 
     }
 
 
 
-        public void setArray() throws IOException{
+        public static void setArray() throws IOException{
             
             
             File file = new File("C:\\Packages\\TASK_SD\\devyTEXT.txt");
@@ -75,6 +82,8 @@ public class VIEW extends JFrame{
                     JOptionPane.showMessageDialog(null, "Could not load data from the file!", "FileNotFoundException", JOptionPane.ERROR);
 
                 }
+                filein.close();
+
     
     
     }
@@ -82,7 +91,7 @@ public class VIEW extends JFrame{
         public void setPanel1(){
 
             panel = new JPanel();
-            elementList = new JList(selected);
+            elementList = new JList<>(selected);
             elementList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
             elementList.setVisibleRowCount(8);
             scrollPane1 = new JScrollPane(elementList);
@@ -92,9 +101,10 @@ public class VIEW extends JFrame{
 
         public void setPanel2(){
 
+            model.addElement("Default value");
             panel2 = new JPanel();
-            selectedElementList = new JList();
-            selectedElementList.setVisibleRowCount(8);
+            selectedElementList = new JList<>(model);
+            selectedElementList.setVisibleRowCount(8);;
             scrollPane2 = new JScrollPane(selectedElementList);
             panel2.add(scrollPane2);
 
@@ -113,22 +123,27 @@ public class VIEW extends JFrame{
 
             public void actionPerformed(ActionEvent e){
 
-                Object[] selections = elementList.getSelectedValues();
-                selectedElementList.setListData(selections);
+                
+                model.addElement(elementList.getSelectedValue());
 
+            
             }
 
         }
 
         public static void main(String[] args) {
             
+            JOptionPane.showMessageDialog(null, "Thread is running", "alert", JOptionPane.ERROR_MESSAGE);
 
+           
             try{
+                setArray();
             new VIEW();
             }
             catch(IOException e){
 
-                System.out.println(e.getMessage());
+                JOptionPane.showMessageDialog(null, e.getMessage(), "alert", JOptionPane.ERROR_MESSAGE);
+
 
             }
 
